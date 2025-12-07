b530b34ac377553be59fbcb85a142339c0a783d3
Devanathan Harikrishnan <devanathan4444@gmail.com>
2025-12-07 17:35:34 +0530

    version test

--- Instagram Unfollow Helper.user.js
+++ Instagram Unfollow Helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram Unfollow Helper
 // @namespace    https://deva-ig-unfollow-helper
-// @version      1.1
+// @version      1.10
 // @description  Safely unfollow with delays.
 // @author       deva-hari
 // @match        https://www.instagram.com/*
